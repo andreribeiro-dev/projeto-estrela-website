e9af31499c701ebd12b4e19e071a821e369bcc68
andreribeiro-dev <andreribeiro.dev@gmail.com>
2024-06-30 10:56:49 -0300

    Navbar finished (media queries)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,41 @@
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import Logo from '../images/logo.png'
 import {links} from '../data'
 import { FaBars } from "react-icons/fa6";
+import { MdOutlineClose } from "react-icons/md";
 import './navbar.css'
 
 
 
 const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
+
+
+
+
   return (
     <nav>
         <div className="container nav_container">
            <Link to= "/" className='logo'>
             <img src={Logo} alt="Nav Logo" />
            </Link>
-            <ul className='nav_links'> 
+            <ul className={`nav_links ${isNavShowing ? 'show_nav' : 'hide_nav'}`}>  
                 {
                     links.map(({name, path}, index) => {
                         return (
-                            <li>
+                            <li key={index}>
                                 <NavLink to={path}>{name}</NavLink>
                             </li>
                         )
                     })
                 }
             </ul>
-            <button className="nav_toggle-btn">
-                <FaBars />
+            <button className="nav_toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+                {
+                    isNavShowing ? <MdOutlineClose/> : <FaBars/> 
+
+                }
             </button>
         </div>
     </nav>
