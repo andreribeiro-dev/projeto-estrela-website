a3f6842f265ed9cbacc790a3f3c9b96836230f20
andreribeiro-dev <andreribeiro.dev@gmail.com>
2024-07-05 12:15:49 -0300

    Create Hero Image
    Imagem Hero da Página Inicial

--- src/components/mainHeader.jsx
+++ src/components/mainHeader.jsx
@@ -1,10 +1,18 @@
+import React from 'react';
+import HeroImage from '../images/hero.jpg';
 
-
-
-const Header = () => {
+import './mainHeader.css'
+const MainHeader = () => {
   return (
-    <header className="main_header"></header>
-  )
+    <header className="main_header">
+      <div className='hero-container'>
+        <img src={HeroImage} alt="Hero Image" className='hero-image' />
+        <div>
+            {/* Conteúdo adicional do hero, como título, texto descritivo, etc. */}
+        </div>
+      </div>
+      </header>
+  );
 }
 
-export default Header
+export default MainHeader
