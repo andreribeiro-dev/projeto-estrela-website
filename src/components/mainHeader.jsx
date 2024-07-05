82132f4e5cba3ff2d8e1f16c67f9a265b0817705
andreribeiro-dev <andreribeiro.dev@gmail.com>
2024-07-05 14:53:53 -0300

    refactor: Hero Image and mainHeader
    Inclusão de Texto da Hero e botão redirecionador para página "Sobre o Projeto"
    
    Date:      Fri Jul 5 15:05:53 2024 -0300
     On branch main
     Your branch and 'origin/main' have diverged,
    Changes to be committed:
    	modified:   src/components/mainHeader.css
    	modified:   src/components/mainHeader.jsx
    	modified:   src/components/navbar.css

--- src/components/mainHeader.jsx
+++ src/components/mainHeader.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import HeroImage from '../images/hero.jpg';
+import { Link } from 'react-router-dom';
+import HeroImage from '../images/hero.jpg'
 
 import './mainHeader.css'
 const MainHeader = () => {
   return (
     <header className="main_header">
-      <div className='hero-container'>
-        <img src={HeroImage} alt="Hero Image" className='hero-image' />
-        <div>
-            {/* Conteúdo adicional do hero, como título, texto descritivo, etc. */}
+      <div className='hero_container'>
+        <div className='hero_image' style={{backgroundImage:`url($(HeroImage})`}}>
+          <div className='hero_text'>
+              <h1>Projeto Estrela</h1>
+              <p>Um projeto social para crianças e adolescentes de baixa renda que concilia educação e futebol de forma gratuita. </p>
+              <button className='hero_btn'>
+                <Link to="/about" className='text_hero_btn'>Sobre o projeto</Link>
+              </button>
+          </div>
         </div>
       </div>
       </header>
