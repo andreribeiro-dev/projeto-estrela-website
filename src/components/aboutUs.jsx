4a1663af9026343f399a97a8087dfd12deea9bc0
andreribeiro-dev <andreribeiro.dev@gmail.com>
2024-07-29 22:01:44 -0300

    Update aboutUs.jsx

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -8,7 +8,7 @@ const AboutUs = () => {
       <div className="container_aboutUs">
         <div className="aboutUs_content">
           <div className="aboutUs_text">
-            <h3>Sobre Nós</h3>
+            <h3>Sobre o Projeto</h3>
             <h2>Nossa Missão</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus dolorum eligendi provident ullam repellat similique.</p>
             <Link to="/about" className="aboutUs_btn">Saiba Mais</Link>
